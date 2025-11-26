0b971a42c614a25c2b722efb192dac66bd581265
pichu2707 <pichu2707@gmail.com>
2025-11-26 22:59:48 +0000

    Subiendo open graph y arreglo de problema con opengraph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import opengraphImage from 'astro-opengraph-images'
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        opengraphImage()
-    ],
 });
